fcad8b36dbfd4de398467471bacf3668eb77e581
Ejalepaco <fdominguezp@gmail.com>
2023-12-08 12:23:50 +0100

    claseDate

---
+++ Programas/src/ClaseDate.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class ClaseDate {
+
+
+    public static void main(String[] args) {
+
+        Date fechaActual = new Date();
+
+        System.out.println("La fecha actual es: "+fechaActual);
+
+    }
+}
